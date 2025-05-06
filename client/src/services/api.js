2e80674cfebfc798f2f56ec6ae41c29c5e05c1be
crlsswhspr <petrechko.pn@ucu.edu.ua>
2025-05-06 09:59:04 +0300

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,1 +1,26 @@
+/*
+src/services/api.js
+*/
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  timeout: 10000,
+});
+
+api.interceptors.request.use(cfg => {
+  const token = localStorage.getItem('authToken');
+  if (token) cfg.headers.Authorization = `Bearer ${token}`;
+  return cfg;
+});
+
+export const createAccount   = data => api.post('/users', data);
+export const submitTest      = answers => api.post('/tests', { answers });
+export const fetchTherapists = () => api.get('/therapists');
+export const fetchProfile    = () => api.get('/users/me');
+export const updateProfile   = data => api.put('/users/me', data);
+
+export default api;
+
 
